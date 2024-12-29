5b7e4845385e3f9b5ca2b06e6231d5351779c195
SupunNarangoda <supun.vishva.narangoda@gmail.com>
2024-12-29 22:12:32 +0530

    Percentage Update

--- src/PaymentStratergy/LoyaltyProgram.java
+++ src/PaymentStratergy/LoyaltyProgram.java
@@ -4,7 +4,7 @@ public class LoyaltyProgram {
     private int points = 0;
 
     public void addPoints(int amount) {
-        points += (int) (amount*0.4);
+        points += (int) (amount*0.2);
     }
 
     public int getPoints() {
