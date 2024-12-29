bce3d50788924eead75c0e4531ca68082aa03273
SupunNarangoda <supun.vishva.narangoda@gmail.com>
2024-12-29 12:26:06 +0530

    Removed Redundant Code

--- src/Command/SubmitFeedbackCommand.java
+++ src/Command/SubmitFeedbackCommand.java
@@ -1,29 +1,3 @@
-//package Command;
-//import OrderObserver.Order;
-//
-//public class SubmitFeedbackCommand implements Command {
-//    private Order order;
-//    private String feedback;
-//
-//    public SubmitFeedbackCommand(Order order, String feedback) {
-//        this.order = order;
-//        this.feedback = feedback;
-//    }
-//
-//    @Override
-//    public void execute() {
-//        System.out.println("Feedback submitted for order: " + order.getPizza().getDescription());
-//        System.out.println("Feedback: " + feedback);
-//        // Here you can add logic to save the feedback to a database or log.
-//    }
-//
-//    @Override
-//    public void undo() {
-//        System.out.println("Feedback for order " + order.getPizza().getDescription() + " has been removed.");
-//        // Here you can add logic to remove the feedback from the database or log.
-//    }
-//}
-
 package Command;
 
 import Feedback.*;
